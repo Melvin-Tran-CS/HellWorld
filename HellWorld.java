49d43d3d1f45ccf18b5e806688fa7278ebadb2eb
Melvin-Tran-CS <m.tran14@share.epsb.ca>
2018-12-11 12:26:15 -0700

    Add files via upload

---
+++ HellWorld.java
@@ -0,0 +1,32 @@
+/**<h>I have osteoperosis</h1>
+*<b>Purpose of Project:</b><br>
+*<p>To give people depression</p>
+*<b>How to start the project instructions:</b><br>
+*<p>I dont know</p>
+*<b>User Instructions:<b/><br>
+*<p>You have the big homo</P>
+*<br>
+*@author Melvin Tran
+*@version 1
+*@since 2018-12-11
+*/
+
+public class HellWorld {
+  /**<b>Class Description: Necessary Class</b><br>
+  *<p>I dont know</p>
+  *@param
+  *@return
+  *@throws
+  */
+
+  /**<b>Method Description: Main, Driver</b><br>
+  *<p>I dont know</p>
+  *@param args unused
+  *@return
+  *@throws
+  */
+
+   public static void main(String[] args) {
+	System.out.println("I have the crippling gay");
+   }
+}

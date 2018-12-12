73298940982fd1dd6c01545cba52c3f1a1c91802
Melvin-Tran-CS <m.tran14@share.epsb.ca>
2018-12-12 12:19:15 -0700

    SMASH

--- HellWorld.java
+++ HellWorld.java
@@ -27,6 +27,58 @@ public class HellWorld {
   */
 
    public static void main(String[] args) {
-	System.out.println("I have Osteoporosis");
+	
+System.out.println("Colors weave into a spire of flame");
+System.out.println("Distant sparks call to a past still unnamed");
+System.out.println("Bear thiss torch against the cold of the night");
+System.out.println("Search your soul and reawaken the undying light");
+
+
+System.out.println("On that day, when the sky fell away");
+System.out.println("Our world came to an end");
+System.out.println("In our eyes did a fading sun rise");
+System.out.println("In the dark, glimmering shadows");
+System.out.println("Silence grows, in the spaces between");
+System.out.println("Stretching out beyond time");
+System.out.println("Rising up, as a chorus of souls finds a voice");
+System.out.println("Flickering through the vvoid");
+System.out.println("These little sparks cling on to life");
+System.out.println("Everyone caught in the struggle");
+System.out.println("And then the storms of change, they fan the flames");
+System.out.println("Scattering ashes to the wind");
+
+
+System.out.println("Every soul contains a whisper of light");
+System.out.println("Gleaming faintly as it dwindles from sight");
+System.out.println("No escape, no greater fate to be made");
+System.out.println("In the end, the chains of time will not breakk");
+
+
+System.out.println("Colors weave into a spire of flame");
+System.out.println("Distant sparks call to a past still unnamed");
+System.out.println("Bear thiss torch against the cold of the night");
+System.out.println("Search your soul and reawaken the undying light");
+
+
+System.out.println("As fate spins a thread without end");
+System.out.println("New life draws its first breath");
+System.out.println("Blossoming in a soil reclaimed from the past");
+System.out.println("Where destiny holds fast");
+System.out.println("Here where we stand, hand clenched in hand");
+System.out.println("Everyone caught in the struggle");
+System.out.println("This is the day we finallyy find our way");
+System.out.println("Stepping into our tomorrow");
+
+
+System.out.println("Every soul contains a whisper of light");
+System.out.println("Growing louder as it calls to unite");
+System.out.println("From the distance sings a chorus of souls");
+System.out.println("Rising slowly, stirring heat from the coals");
+
+
+System.out.println("Colors weave into a spire of flame");
+System.out.println("Distant sparks call to a past still unnamed");
+System.out.println("Bear thiss torch against the cold of the night");
+System.out.println("Light will guide you on your way to the ultimate fight");
    }
 }
